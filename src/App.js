58f21e5e48f6503ec2559b7b9521d11eb5a25585
Bhagya Kavati <56948344+kavatibhagya@users.noreply.github.com>
2020-06-22 17:54:32 -0700

    adding friuts and veggies

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import './App.css';
 import MyApp from './components/Welcome';
 
+function App() {
+  return (
+    <>
+      <div className="App friutes-header">
+        <MyApp name="Apple" image="/images/apple.jpg" />
+        <MyApp name="kiwi" image="/images/Kiwi.jpg" />
+        <MyApp name="Orange" image="/images/Orange.jpeg" />
+        <MyApp name="Strawberry" image="/images/strawberry.png" />
+        <MyApp name="Lemon" image="/images/Lemon.png" />
 
-              
+      </div>
+      <br></br>
+      <div className="App friutes-header">
+        <MyApp name="Tomato" image="/images/apple.jpg" />
+        <MyApp name="Carrots" image="/images/Kiwi.jpg" />
+        <MyApp name="Beans" image="/images/Orange.jpeg" />
+        <MyApp name="Potato" image="/images/strawberry.png" />
+        <MyApp name="Cucumber" image="/images/Lemon.png" />
 
-
-
-function App(){
-    return (
-    <div className="App">
-      <MyApp/>
-    </div>
+      </div>
+    </>
   );
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './Welcome.css'
 
-function MyApp(){
+function MyApp(props) {
      return (
-         <div className ="friutes-header">
-           <div><img src={require('../images/apple.jpg')}  alt="apple" className ="friut" />
-           <p>Apple</p>
-           </div>
-            <div> <img src={require('../images/Lemon.png')} alt="Lemon"className ="friut" /><p>Lemon</p></div>
-            <div> <img src={require('../images/Orange.jpeg')} alt="Orange" className ="friut" /><p>Orange</p></div>
-            <div> <img src={require('../images/strawberry.png')} alt="strawerry" className ="friut" /><p>strawberry</p></div>
-            <div>  <img src={require('../images/Kiwi.jpg')} alt="kiwi" className ="friut" /><p>Kiwi</p></div>
-         </div>
+     
+               <div >
+                    <img src={props.image} alt={props.name} className="friut" />
+                   <p className="p"> {props.name} </p>
+               </div>
+              
+
+          //  <div className ="friutes-header">
+          //    <div><img src={require('../images/apple.jpg')}  alt="apple" className ="friut" />
+          //    <p>Apple</p>
+          //    </div>
+          //     <div> <img src={require('../images/Lemon.png')} alt="Lemon"className ="friut" /><p>Lemon</p></div>
+          //     <div> <img src={require('../images/Orange.jpeg')} alt="Orange" className ="friut" /><p>Orange</p></div>
+          //     <div> <img src={require('../images/strawberry.png')} alt="strawerry" className ="friut" /><p>strawberry</p></div>
+          //     <div>  <img src={require('../images/Kiwi.jpg')} alt="kiwi" className ="friut" /><p>Kiwi</p></div>
+          //  </div>
 
      );
 }
